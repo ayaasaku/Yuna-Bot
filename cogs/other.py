45520bd0c67fbb997871761ca4ffd88da70779ef
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2023-04-26 15:30:17 +0800

    fix

--- cogs/other.py
+++ cogs/other.py
@@ -4,9 +4,6 @@ from discord.ext import commands
 
 class OtherCog(commands.Cog, name='other'):
     def __init__(self, bot):
-        self.bot = bot
-        
-            
-            
+        self.bot = bot       
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(OtherCog(bot))
