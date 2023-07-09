cde037f1657bd5964ca4703dad5286fea9a8c795
Ayaakaa <kamisatoayaakaa1127@gmail.com>
2023-07-09 16:55:08 +0800

    fix

--- cogs/main.py
+++ cogs/main.py
@@ -12,7 +12,7 @@ class MainCog(commands.Cog, name='main'):
     @app_commands.command(name='about', description='有關三月七')
     async def about(self, interaction: Interaction):
         embed = defaultEmbed(title="三月七 • March 7th Bot",
-                             description="**三月七**是由**綾霞**製作的機器人")
+                             description="**三月七**是由綾霞製作的機器人")
         embed.set_author(name="三月七", url="https://github.com/Ayaakaa/March-7th-Bot",
                          icon_url="https://i.imgur.com/Zp9bgVN.jpeg")
         embed.set_image(url="https://i.imgur.com/sU7bXs1.jpeg")
@@ -25,17 +25,14 @@ class MainCog(commands.Cog, name='main'):
         tuple2 = ('三月七:','小三月:','三月:')
         if msg.author.id == 831883841417248778 and msg.content[0:3] in tuple1 or msg.content[0:3] in tuple2 or msg.content[0:4] in tuple1 or msg.content[0:4] in tuple2:
             global text
-            
             if msg.content[0:3] or msg.content[0:4] in tuple1: text = msg.content.split('：')[1]
             elif msg.content[0:3] or msg.content[0:4] in tuple2: text = msg.content.split(': ')[1]
             else: return
-            
             if msg.reference != None or msg.reference is not None:
                 reply_id = msg.reference.message_id
                 await msg.delete()
                 reply_message = await msg.channel.fetch_message(reply_id)
                 await reply_message.reply(text)
-                
             else:
                 await msg.delete()
                 await msg.channel.send(text)
